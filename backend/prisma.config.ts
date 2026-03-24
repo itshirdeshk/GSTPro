68c4227e848ae444bdda258528754051ed470a15
itshirdeshkkk <hirdesh.khandelwal@tedekstra.com>
2026-03-24 23:28:44 +0530

    fix: add trust proxy

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: "postgresql://postgres:ZTjmzFttRpufMBomQydNbDDLHbmWqtwQ@postgres.railway.internal:5432/railway",
   },
 });
